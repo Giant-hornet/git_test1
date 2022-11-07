e6c74fb113a134b9b5a30d79b1acfef6effc4fda
Giant-hornet <jhnam0816@naver.com>
2022-11-08 00:51:05 +0900

    Add growl [Mul]

--- growl.c
+++ growl.c
@@ -2,6 +2,7 @@
 
 int add(int a, int b);
 int sub(int a, int b);
+int mul(int a, int b);
 
 int main() {
 	int a = 10;
@@ -16,4 +17,7 @@ int add(int a, int b) {
 }
 int sub(int a, int b) {
 	return a - b;
+}
+int mul(int a, int b) {
+	return a * b;
 }
